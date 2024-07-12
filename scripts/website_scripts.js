78e61d08c2d4fad10c00e39c79a043dd275350fc
CircusBirkus <birk.endruhn@edu.sait.ca>
2024-07-12 17:14:15 -0600

    Add files via upload

--- scripts/website_scripts.js
+++ scripts/website_scripts.js
@@ -12,3 +12,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var accordions = document.querySelectorAll('.accordion');
+
+  accordions.forEach(function(accordion) {
+      accordion.addEventListener('click', function() {
+          this.classList.toggle('active');
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+          } else {
+              panel.style.maxHeight = panel.scrollHeight + 'px';
+          }
+      });
+  });
+});
